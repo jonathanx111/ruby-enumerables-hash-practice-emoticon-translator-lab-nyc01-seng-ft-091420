6388eeb33f9936c5c6b3238dafcaf8c10c9064c0
Jonathan Xiao <jonathanx111@gmail.com>
2020-09-07 14:09:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,6 +25,8 @@ def get_english_meaning(file, emoticon)
     v.each do |inner|
       if v[:japanese] == emoticon
         return k
+      else
+        puts "Sorry, that emoticon was not found"
       end
     end
   end
